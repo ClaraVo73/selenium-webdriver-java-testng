555a9c6c9364fad4e06cbb82e8fcdca2ecaff331
ClaraVo73 <nganhaqb92@gmail.com>
2024-06-17 10:13:46 +0700

    Add topic04 Run on browser

---
+++ selenium/webdriver/Topic_03_Check_Environment.java
@@ -0,0 +1,34 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Topic_03_Check_Environment {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    @Test
+    public void TC_01_Run_On_Firefox() {
+        driver = new FirefoxDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC_02_Run_On_Chrome() {
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC_03_Run_On_Edge() {
+        driver = new EdgeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+}
---
+++ selenium/webdriver/Topic_04_Run_On_Browser.java
@@ -0,0 +1,61 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Topic_04_Run_On_Browser {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    //chrome
+    @Test
+    public void TC01_Run_Chrome() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+        } else {
+            System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+        }
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://flukeout.github.io/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC02_Run_Firefox() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        }
+
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://flukeout.github.io/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC03_Run_Edge() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+        } else {
+            System.setProperty("webdriver.edge.driver", projectPath + "/browserDrivers/msedgedriver");
+        }
+
+        driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://flukeout.github.io/");
+        driver.quit();
+    }
+
+
+
+}

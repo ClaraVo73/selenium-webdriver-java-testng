6454d77bb6c7ba2029f87478897281974601e918
ClaraVo73 <nganhaqb92@gmail.com>
2024-06-19 14:49:57 +0700

    Add topic06 Web Element

---
+++ selenium/webdriver/Topic_06_Login.java
@@ -0,0 +1,94 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Topic_06_Login {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+        } else {
+            System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+        }
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void tc01_empty_email_and_password(){
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+        sleepInSecond(2);
+
+        driver.findElement(By.id("send2")).click();
+
+        Assert.assertEquals(driver.findElement(By.id("advice-required-entry-email")).getText(),"This is a required field.");
+        Assert.assertEquals(driver.findElement(By.id("advice-required-entry-pass")).getText(),"This is a required field.");
+    }
+
+    @Test
+    public void tc02_invalid_email(){
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+        sleepInSecond(2);
+
+        driver.findElement(By.id("email")).sendKeys("1234@1234.123");
+        driver.findElement(By.id("pass")).sendKeys("123456");
+        driver.findElement(By.id("send2")).click();
+
+        Assert.assertEquals(driver.findElement(By.id("advice-validate-email-email")).getText(),"Please enter a valid email address. For example johndoe@domain.com.");
+    }
+
+    @Test
+    public void tc03_verify_password(){
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+        sleepInSecond(2);
+
+        driver.findElement(By.id("email")).sendKeys("auto@gmail.com");
+        driver.findElement(By.id("pass")).sendKeys("12345");
+        driver.findElement(By.id("send2")).click();
+
+        Assert.assertEquals(driver.findElement(By.id("advice-validate-password-pass")).getText(),"Please enter 6 or more characters without leading or trailing spaces.");
+    }
+
+    @Test
+    public void tc04_incorrect_email_password(){
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+        sleepInSecond(2);
+
+        driver.findElement(By.id("email")).sendKeys("auto@gmail.com");
+        driver.findElement(By.id("pass")).sendKeys("12345678");
+        driver.findElement(By.id("send2")).click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//li[@class='error-msg']//span")).getText(),"Invalid login or password.");
+    }
+
+
+
+    public void  sleepInSecond(long timeInSecond){
+        try{
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}

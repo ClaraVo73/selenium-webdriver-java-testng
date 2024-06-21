944ae6e0ee091007c2b57600df3be4f51ef99f09
ClaraVo73 <nganhaqb92@gmail.com>
2024-06-21 07:17:30 +0700

    update testcase 05, 06 of Topic06 Login

--- selenium/webdriver/Topic_06_Random_Email.java
+++ selenium/webdriver/Topic_06_Random_Email.java
@@ -1,61 +1,19 @@
 package webdriver;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 public class Topic_06_Random_Email {
-    WebDriver driver;
-    Random rand;
-    String projectPath = System.getProperty("user.dir");
-    String osName = System.getProperty("os.name");
-    String emailAddress;
+   public static void main(String[] args){
+       //untilities
+       //Data type: Class/interface / collection/ String/ Float
+       Random rand = new Random();
+       System.out.print(rand.nextFloat());
+       System.out.print(rand.nextDouble());
+       System.out.print(rand.nextInt());
+       System.out.print(rand.nextInt(999999));
+       System.out.print("automation" + rand.nextFloat() + "@gmail.com");
+       System.out.print(rand.nextLong());
 
-    @BeforeClass
-    public void beforeClass() {
-        if (osName.contains("Windows")) {
-            System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
-        } else {
-            System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
-        }
-        rand = new Random();
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-        emailAddress = "automation" + rand.nextInt(9999) + "@gmail.com";
-    }
-
-   // @Test
-    public void tc04_incorrect_email_password(){
-        driver.get("http://live.techpanda.org/");
-        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
-        sleepInSecond(2);
-
-        driver.findElement(By.id("email")).sendKeys(emailAddress);
-        driver.findElement(By.id("pass")).sendKeys("12345678");
-        driver.findElement(By.id("send2")).click();
-
-        Assert.assertEquals(driver.findElement(By.xpath("//li[@class='error-msg']//span")).getText(),"Invalid login or password.");
-    }
-
-
-    public void  sleepInSecond(long timeInSecond){
-        try{
-            Thread.sleep(timeInSecond * 1000);
-        } catch (InterruptedException e){
-            e.printStackTrace();
-        }
-    }
-    @AfterClass
-    public void afterClass() {
-
-        //driver.quit();
-    }
+   }
 }

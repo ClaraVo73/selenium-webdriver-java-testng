1c4ef1765448f0c8ea0668bd6c7c73c3a2785c1a
ClaraVo73 <nganhaqb92@gmail.com>
2024-06-24 15:37:11 +0700

    Add new topic 10 button

--- selenium/webdriver/Topic_10_Button.java
+++ selenium/webdriver/Topic_10_Button.java
@@ -2,6 +2,7 @@ package webdriver;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -9,6 +10,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.awt.*;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Topic_10_Button {
@@ -28,7 +30,7 @@ public class Topic_10_Button {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
-    @Test
+  //  @Test
     public void tc01_button(){
         //1. Truy cap trang
         driver.get("https://www.fahasa.com/customer/account/create");
@@ -61,6 +63,68 @@ public class Topic_10_Button {
         Assert.assertEquals(driver.findElement(loginButton).getCssValue("background-color"),rgbaColor);
 
     }
+
+    @Test
+    public void tc02_checkbox_radiobutton(){
+        //1. Truy cap trang
+        driver.get("https://demos.telerik.com/kendo-ui/checkbox/index");
+        sleepInSecond(10);
+        //2. Click vao checkbox Dual-zone air conditioning va Kiem tra checkbox do da chọn
+        checkToCheckBox(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::span/input"));
+        Assert.assertTrue(driver.findElement(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::span/input")).isSelected());
+
+
+        //4. Bo chon checkbox va kiem tra no chua duoc chọn
+        unCheckToCheckBox(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::span/input"));
+        Assert.assertFalse(driver.findElement(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::span/input")).isSelected());
+
+        //5. Truy cap vao trang
+        driver.get("https://demos.telerik.com/kendo-ui/radiobutton/index");
+        //6. Click vao radio button: 2.0 Petrol, 147kW
+        checkToCheckBox(By.xpath("//label[contains(text(),'2.0 Petrol, 147kW')]/preceding-sibling::span/input"));
+        Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'2.0 Petrol, 147kW')]/preceding-sibling::span/input")).isSelected());
+
+    }
+
+    public void checkToCheckBox(By by){
+        if (!driver.findElement(by).isSelected()){
+            driver.findElement(by).click();
+        }
+    }
+
+    public void unCheckToCheckBox(By by){
+        if (driver.findElement(by).isSelected()){
+            driver.findElement(by).click();
+        }
+    }
+
+    @Test
+    public void tc03_checkbox_multiple(){
+        //1. Truy cap trang
+        driver.get("https://automationfc.github.io/multiple-fields/");
+        //2. Chon het tat ca checkbox o man hinh nay
+        List<WebElement> allCheckboxes = driver.findElements(By.cssSelector("input.form-checkbox"));
+
+        for (WebElement checkbox : allCheckboxes){
+            checkbox.click();
+            sleepInSecond(1);
+        }
+
+        //3. Kiem tra het tat ca checkbox do da chon
+
+        for (WebElement checkbox : allCheckboxes){
+            Assert.assertTrue(checkbox.isSelected());
+        }
+        //4. Chi chon mot checkbox trong tat ca cac checkbox
+        for (WebElement checkbox : allCheckboxes){
+            if (checkbox.getAttribute("value").equals("Cancer")){
+                checkbox.click();
+            }
+        }
+        //5. Kiem tra duy nhat checkbox nay da chon
+
+
+    }
     public void  sleepInSecond(long timeInSecond){
         try{
             Thread.sleep(timeInSecond * 1000);
@@ -75,6 +139,6 @@ public class Topic_10_Button {
 
     @AfterClass
     public void afterClass() {
-        driver.quit();
+       // driver.quit();
     }
 }

92cf077d18208064092a58757b4249a83af8a6fc
ClaraVo73 <nganhaqb92@gmail.com>
2024-06-26 08:02:54 +0700

    Add new topic 13

---
+++ selenium/webdriver/Topic_13_User_Interaction.java
@@ -0,0 +1,43 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Topic_13_User_Interaction {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+        } else {
+            System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+        }
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void tc01(){
+
+    }
+    public void  sleepInSecond(long timeInSecond){
+        try{
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}

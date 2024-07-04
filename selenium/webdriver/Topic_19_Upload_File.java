309c2922a30a0c72d8339301a35290e3e96db91d
ClaraVo73 <nganhaqb92@gmail.com>
2024-07-04 15:00:16 +0700

    update topic 19 _ upload file

--- selenium/webdriver/Topic_19_Upload_File.java
+++ selenium/webdriver/Topic_19_Upload_File.java
@@ -1,17 +1,24 @@
 package webdriver;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Topic_19_Upload_File {
     WebDriver driver;
     String projectPath = System.getProperty("user.dir");
     String osName = System.getProperty("os.name");
+    JavascriptExecutor jsExecutor;
+
 
     String beachFile = "beach.jpg";
     String sunFile = "sun.jpg";
@@ -31,6 +38,7 @@ public class Topic_19_Upload_File {
         }
 
         driver = new ChromeDriver();
+        jsExecutor = (JavascriptExecutor) driver;
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
@@ -38,10 +46,34 @@ public class Topic_19_Upload_File {
     public void tc01_by_sendkeys(){
         //1. access page
         driver.get("https://blueimp.github.io/jQuery-File-Upload/");
+
         //2. Upload 3 files
+        driver.findElement(By.xpath("//input[@name='files[]']")).sendKeys(beachFilePath + "\n" + sunFilePath + "\n" + townFilePath);
+        sleepInSecond(1);
+
         //3. Check upload file successfully
+        Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='"+ beachFile + "']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='"+ sunFile + "']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='"+ townFile + "']")).isDisplayed());
+
         //4. CLick on Start button of files
-        //5. Verify successful upload 3 files
+        List<WebElement> buttonUpload = driver.findElements(By.cssSelector("table button.start"));
+        for (WebElement button : buttonUpload){
+            button.click();
+            sleepInSecond(3);
+        }
+
+        //5. Verify successful upload 3 files by link
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()='"+ sunFile +"']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()='"+ beachFile +"']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()='"+ townFile +"']")).isDisplayed());
+
+        //6. Verify successful upload 3 files by images
+        Assert.assertTrue(isImageLoaded("//img[contains(@src,'" + sunFile +"')]"));
+        Assert.assertTrue(isImageLoaded("//img[contains(@src,'" + beachFile +"')]"));
+        Assert.assertTrue(isImageLoaded("//img[contains(@src,'" + townFile +"')]"));
+
+
 
     }
     public void  sleepInSecond(long timeInSecond){
@@ -51,8 +83,18 @@ public class Topic_19_Upload_File {
             e.printStackTrace();
         }
     }
+
+    public boolean isImageLoaded(String locator) {
+        WebElement element = getElement(locator);
+        boolean status = (boolean) jsExecutor.executeScript(
+                "return arguments[0].complete && typeof arguments[0].naturalWidth != 'undefined' && arguments[0].naturalWidth > 0", element);
+        return status;
+    }
+    public WebElement getElement(String locator) {
+        return driver.findElement(By.xpath(locator));  // Adjust this based on your locator strategy
+    }
     @AfterClass
     public void afterClass() {
-        driver.quit();
+       // driver.quit();
     }
 }
